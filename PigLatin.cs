84b0b82cf32e20c0cbb686858a89c90c78e7e453
Joshua <ezejoshuac@gmail.com>
2022-09-06 03:41:10 +0100

    Add pseudocode for piglatin program

---
+++ PigLatin.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bezao_technical
+{
+    public class PigLatin
+    {
+        // static void Main(string[] args)
+        public static void generatePigLatin()
+        {
+            // Push words in string into a list
+            // for each word in the list, return characters after the first character
+            // concatenate chars with the first character and "ay" 
+
+        }
+    }
+}
