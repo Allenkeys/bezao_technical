7713abe6d3b06beb48e076562c612e6b98b81a6d
Joshua <ezejoshuac@gmail.com>
2022-09-06 03:40:05 +0100

    Create main entry class

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bezao_technical;
+
+namespace bezao_technical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Leapyear.LeapYears();
+            // GuessingGame.guess();
+            // PigLatin.generatePigLatin()
+        }
+    }
+}
